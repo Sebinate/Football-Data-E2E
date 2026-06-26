d1b1884a04c156bf749904dedcebc7af7f63d978
Seb Astian <sebastiansampao@gmail.com>
2026-06-26 17:21:55 +0800

    Added extract functionality

---
+++ infra/extract/extract.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import boto3
+from infra.logger.logger import logging
+
+def extract_api(path: str, api_key: str = None):
+    try:
+        if api_key:
+            header = {'x-apisport-key': api_key}
+
+        else:
+            header = {}
+
+        response = requests.get(path, headers=header)
+        data = response.json()
+        logging.info('Successfully requested data')
+
+        return data
+    
+    except Exception as e:
+        logging.error('Fatally errored in requesting data')
+        raise e
+
+def store_to_s3_bronze(data, bucket_name: str, key: str):
+    try:
+        s3 = boto3.client('s3')
+        logging.info('Established connection to S3')
+
+        s3.put_object(
+            Bucket=bucket_name,
+            Key=key,
+            Body=json.dumps(data)
+        )
+        logging.info('Successfully ')
+
+    except Exception as e:
+        logging.error(f'Fatally errored in Loading data into S3 bucket')
+        raise e
